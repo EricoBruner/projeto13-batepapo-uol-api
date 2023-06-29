414761d732ecd6c7aa7386f987b29902c1aafeff
EricoBruner <ericodasilvabruner@gmail.com>
2023-06-28 22:50:10 -0300

    feito função de cadastrar mensagens

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -40,4 +40,26 @@ router.get("/participants", async (req, res) => {
   }
 });
 
+router.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+
+  try {
+    const resp = await db.collection("participants").findOne({ name: user });
+    if (!resp) return res.sendStatus(422);
+
+    await db.collection("messages").insertOne({
+      from: user,
+      to,
+      text,
+      type,
+      time: dayjs(Date.now()).format("HH:mm:ss"),
+    });
+
+    return res.sendStatus(201);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
 export default router;
